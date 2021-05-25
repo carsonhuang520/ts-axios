0abff0e03657280aba102cdcd92610d93373ac63
carsonhuang <981347807@qq.com>
2021-05-26 00:45:23 +0800

    完成处理请求 url 参数

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './type/index'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { params, url } = config
+  return buildURL(url, params)
 }
 
 export default axios
