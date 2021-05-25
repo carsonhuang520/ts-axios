0a27bcc7fcd7cb73ffa0aafae4a710fe99159496
carsonhuang <981347807@qq.com>
2021-05-26 01:01:19 +0800

    完成处理请求 body 数据

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { transformRequest } from './helpers/data'
 import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './type/index'
 import xhr from './xhr'
@@ -9,11 +10,17 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { params, url } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): string {
+  const { data } = config
+  return transformRequest(data)
 }
 
 export default axios
