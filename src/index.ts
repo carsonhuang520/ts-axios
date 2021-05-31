8e66cfad65b505ffb9fc9e85b5fcdc8c9eb6467e
carsonhuang <981347807@qq.com>
2021-06-01 00:28:35 +0800

    完成错误信息增强

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,4 @@
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-import { buildURL } from './helpers/url'
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './type/index'
-import xhr from './xhr'
-
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { params, url } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): string {
-  const { data } = config
-  return transformRequest(data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): string {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+import axios from './axios'
+export * from './type'
 
 export default axios
